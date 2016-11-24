db0e39996fa0dc92cf1f2d5c6d4ae624368481b3
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-11-24 19:48:46 +0530

    added more templates, base.html improved, footer added (c)

--- app/views.py
+++ app/views.py
@@ -1,15 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
 def index():
     user = {'nickname': 'Vasantha Ganesh Kanniappan'}
     return render_template('index.html',
                            title='Home',
                            user=user)
 
-@app.route('/prob')
+@app.route('/p')
 def prob():
     problem = {'id':101,
                'pname':'watermelons',
@@ -17,4 +17,16 @@ def prob():
                'blk': [{'id':1, 'tcase':'hello eon', 'ssoln':'mellow'},
                        {'id':2, 'tcase':'jam bingo', 'ssoln':'vangaugh'}]}
     return render_template('prob.html',
+                           title=problem["id"],
                            problem=problem)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="{}", remember_me={}'.format(form.openid.data,
+                                                                       str(form.remember_me.data)))
+        return redirect('/')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
